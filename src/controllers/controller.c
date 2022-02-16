aa27ae3e619f95ed25974344b5ea39b191bfcd60
dywallac <dywallac@umich.edu>
2022-02-16 12:42:19 -0500

    fix source for yaw and update setting file hover/open loop descent

--- src/controllers/controller.c
+++ src/controllers/controller.c
@@ -419,7 +419,7 @@ static void __run_attitude_controller()
     rc_saturate_double(&setpoint.pitch_dot, -MAX_PITCH_RATE, MAX_PITCH_RATE);
 
     // 3) Yaw -> Yaw Rate
-    setpoint.yaw_dot  = rc_filter_march(&D_yaw,  setpoint.yaw  - state_estimate.yaw)
+    setpoint.yaw_dot  = rc_filter_march(&D_yaw,  setpoint.yaw  - state_estimate.continuous_yaw)
                        + setpoint.yaw_dot_ff;
     rc_saturate_double(&setpoint.yaw_dot, -MAX_YAW_RATE, MAX_YAW_RATE);
 
